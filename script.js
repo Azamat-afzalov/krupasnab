db13a64d72282bb7126e6c22f900aa445ec4f2c4
User <azamatafzalov1006@gmail.com>
2020-11-21 15:17:49 +0500

    added some animations

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ openBtn.addEventListener('click', e => {
 closeBtn.addEventListener('click', e => {
     menu.classList.remove('header__nav--active')
 });
+
+const productsItems = document.querySelectorAll('.products__item');
+let delay = 0;
+productsItems.forEach((item) => {
+    delay = delay + 150;
+    item.setAttribute("data-aos-delay", delay)
+});
+
+const galleryItems = document.querySelectorAll('.gallery__item');
+let galleryDelay = 0;
+galleryItems.forEach((item) => {
+    galleryDelay = galleryDelay + 150;
+    item.setAttribute("data-aos-delay", galleryDelay)
+});
